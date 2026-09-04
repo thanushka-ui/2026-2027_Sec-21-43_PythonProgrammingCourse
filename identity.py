89bacfe23274602bb839ba5efefe698175ef574d
thanushka-ui <thanushka.b99@gmail.com>
2026-09-04 14:21:52 +0530

    identity operator

---
+++ identity.py
@@ -0,0 +1,17 @@
+#identity operators exercise
+a=[1,2,3]
+b=a
+result=b is a #is identify operator 
+print ("result of",b,is a is",result)
+
+
+c=[1,2,3]
+d=[1,2,3]
+result=d is c #is identify operator
+print ("result of",d,is c is",result)
+
+#is not
+a=["1,2,3"]
+b=[1,2,3]
+result=b is not a #is not identify operator
+print ("result of",b,is not a is",result)
