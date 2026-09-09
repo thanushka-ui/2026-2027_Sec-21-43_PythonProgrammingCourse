2a5ef0ae50824465754f2a569046bbc8be2dd4c7
thanushka-ui <thanushka.b99@gmail.com>
2026-09-09 11:32:56 +0530

    add two numbers and read their sum

---
+++ read two numbers.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter num1: "))
+num2=int(input("Enter num2: "))
+result=num1+num2
+print(result)
+
+num1=int(input("Enter num1: "))
+num2=int(input("Enter num2: "))
+result=num1+num2
+print(result)
