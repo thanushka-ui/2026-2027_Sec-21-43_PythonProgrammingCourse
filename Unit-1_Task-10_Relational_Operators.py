9ebdab7421af093d3ebb3eb8a78f3aefe15a8e3f
thanushka-ui <thanushka.b99@gmail.com>
2026-09-18 09:14:27 +0530

    relational operators

---
+++ Unit-1_Task-10_Relational_Operators.py
@@ -0,0 +1,15 @@
+#demonstrate relational operators (print comparision result)
+a=19
+b=22
+result = a==b
+print("result of",a,"==",b,"is:",result)
+result = a!=b
+print("result of",a,"!=",b,"is:",result)
+result= a<b
+print("result of",a,"<",b,"is:",result)
+result =a>b
+print("result of",a,">",b,"is:",result)
+result = a<=b
+print("result of",a,"<=",b,"is:",result)
+result=a>=b
+print("result of",a,">=",b,"is:",result)
