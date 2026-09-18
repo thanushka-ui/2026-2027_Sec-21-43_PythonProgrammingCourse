5d3bca1529daf1062ee084f1fd6f41e077b0495d
thanushka-ui <thanushka.b99@gmail.com>
2026-09-18 14:30:34 +0530

    Delete read two numbers.py

--- read two numbers.py
+++
@@ -1,9 +0,0 @@
-num1=int(input("Enter num1: "))
-num2=int(input("Enter num2: "))
-result=num1+num2
-print(result)
-
-num1=int(input("Enter num1: "))
-num2=int(input("Enter num2: "))
-result=num1+num2
-print(result)
