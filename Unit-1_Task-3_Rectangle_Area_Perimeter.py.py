95e8cb565f8e1bc0b58dccab2081c87254bd1bf4
thanushka-ui <thanushka.b99@gmail.com>
2026-09-18 20:27:14 +0530

    task_3_rectangle_area_perimeter

---
+++ Unit-1_Task-3_Rectangle_Area_Perimeter.py.py
@@ -0,0 +1,7 @@
+#calculate area and perimeter of rectangle 
+lenght=int(input("enter the lenght): "))
+width=int(input("enter the width): "))
+area=lenght*width
+perimeter=2*(lenght+width)
+print("area=", area)
+print("perimeter=", perimeter)
