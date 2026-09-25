f48eeec79557a2e1b7fcb92cf201d327e4f11e39
thanushka-ui <thanushka.b99@gmail.com>
2026-09-25 15:03:12 +0530

    logical operator

---
+++ Unit-1_Task-11_Logical_Operators.py.py
@@ -0,0 +1,8 @@
+#python Logical Operators Exercise
+a = 5
+result = a > 2 and a < 10 #logical and operator
+print("result of",a,"> 2 and", a ,"< 10:",result)#python Logical Operators Exercise
+a = 10
+result = 6 > 4 and a < 10 #logical and operator
+print("result of",a,"> 4 and", a ,"< 10:",result)
+
